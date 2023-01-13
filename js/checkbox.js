55dd0fa927e728c43920f1f9a10cb3081768ec81
Marcos <m.cisternab2022@gmail.com>
2023-01-12 22:45:29 -0300

    Primer commit

---
+++ js/checkbox.js
@@ -0,0 +1,51 @@
+var toggle = document.getElementById('hide-checkbox')
+var body = document.querySelector('body')
+var p = document.querySelector('p')
+var a = document.querySelector('a')
+var h1 = document.querySelector('h1')
+var h2 = document.querySelector('h2')
+var h3 = document.querySelector('h3')
+var h5 = document.querySelector('h5')
+var footer = document.querySelector('footer')
+var hero = document.getElementById('hero')
+var profesion = document.getElementById('profesion')
+var flecha = document.getElementById('flecha')
+var resume = document.getElementById('resume')
+var about = document.getElementById('about')
+var navbar = document.getElementById('navbar')
+var social = document.getElementById('social')
+var portfolio = document.getElementById('portfolio')
+var services = document.getElementById('services')
+var contact = document.getElementById('contact')
+var header = document.getElementById('header')
+var ascensor = document.getElementById('ascensor')
+var info = document.getElementById('info')
+var circle = document.getElementById('circle')
+var curriculum = document.getElementById('curriculum')
+
+toggle.onclick = function(){
+    toggle.classList.toggle('active');
+    body.classList.toggle('active');
+    p.classList.toggle('active');
+    a.classList.toggle('active');
+    h1.classList.toggle('active');
+    h2.classList.toggle('active');
+    h3.classList.toggle('active');
+    h5.classList.toggle('active');
+    hero.classList.toggle('active');
+    profesion.classList.toggle('active');
+    footer.classList.toggle('active');
+    flecha.classList.toggle('active');
+    resume.classList.toggle('active');
+    about.classList.toggle('active');
+    navbar.classList.toggle('active');
+    social.classList.toggle('active');
+    portfolio.classList.toggle('active');
+    services.classList.toggle('active');
+    contact.classList.toggle('active');
+    ascensor.classList.toggle('active');
+    info.classList.toggle('active');
+    curriculum.classList.toggle('active');
+    circle.classList.toggle('active');
+
+}
